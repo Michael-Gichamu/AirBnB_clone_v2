35acd4b0351c338f5b9daad6aa434cffffebcc26
Michael-Gichamu <michaelmatere606@gmail.com>
2023-07-05 14:19:03 +0000

    Compress before sending

---
+++ 1-pack_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Generate a .tgz archive from the contents of web_static folder
+"""
+import os
+from datetime import datetime
+from fabric.api import local, task
+
+
+@task
+def do_pack():
+    """Create a .tgz archive"""
+    time = datetime.now().strftime("%Y%m%d%H%M%S")
+    if not os.path.exists('versions'):
+        local("mkdir versions")
+    tar_name = "versions/web_static_{}.tgz".format(time)
+    result = local("tar -cvzf {} web_static".format(tar_name))
+
+    if result.succeeded:
+        return tar_name
+    else:
+        return None
