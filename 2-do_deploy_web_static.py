e654bdab67e2a26bbfd3ee930ee27c00bc254e31
Michael-Gichamu <michaelmatere606@gmail.com>
2023-07-06 05:20:10 +0000

    Deploy archive!

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+Deploy archive to web servers
+"""
+from os.path import isfile
+from fabric.api import run, put, env, sudo
+
+env.hosts = ['100.25.188.172', '18.209.178.215']
+
+
+def do_deploy(archive_path):
+    """Distributes an archive to web servers"""
+    if not isfile(archive_path):
+        return False
+    try:
+        archive_filename = archive_path.split('/')[-1]
+        archive_no_ext = archive_filename.split('.')[0]
+
+        put(archive_path, '/tmp/')
+
+        sudo('mkdir -p /data/web_static/releases/{}/'.format(archive_no_ext))
+
+        sudo('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
+             .format(archive_filename, archive_no_ext))
+
+        sudo('rm /tmp/{}'.format(archive_filename))
+
+        sudo('mv -f /data/web_static/releases/{}/web_static/* '
+             '/data/web_static/releases/{}/'
+             .format(archive_no_ext, archive_no_ext))
+
+        sudo('rm -rf /data/web_static/releases/{}/web_static'
+             .format(archive_no_ext))
+
+        sudo('rm -rf /data/web_static/current')
+
+        sudo('ln -s /data/web_static/releases/{} '
+             '/data/web_static/current'
+             .format(archive_no_ext))
+
+        return True
+
+    except Exception as e:
+        return False
