b3bac048a715c8e3ca196a6d3c7c00a270182e4e
Michael-Gichamu <michaelmatere606@gmail.com>
2023-07-26 04:29:48 +0000

    Is it a number?

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web app
+Listening on 0.0.0.0, port 5000
+Route:
+    /: display “Hello HBNB!"
+    /hbnb: display “HBNB”
+    /c/<text> - display "C <text>"
+    /python/<text> - display "Python is cool"
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Display 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """display 'HBNB'"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_func(text):
+    """Display “C ” followed by the value of the text variable"""
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_func(text):
+    """Display “Python ”, followed by the value of the text variable"""
+    text = text.replace("_", " ")
+    return f"Python {text}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    if isinstance(n, int):
+        return f"{n} is a number"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
